b3790df04f79a1f6894552db5830422ef0415665
Doka <dokadair@gmail.com>
2022-05-19 17:57:32 +0600

    static

--- ranks_stripe/urls.py
+++ ranks_stripe/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 
@@ -7,3 +9,8 @@ urlpatterns = [
     path('v1/', include('apps.items.urls')),
     path('v2/', include('apps.cart.urls')),
 ]
+
+if settings.DEBUG:
+    if settings.STATIC_ROOT:
+        urlpatterns += static(settings.STATIC_URL,
+                              document_root=settings.STATIC_ROOT)
